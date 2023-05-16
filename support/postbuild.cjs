17d029be5d799bab2cf49ba5258b978b20c0d52f
Eray Hanoğlu <e.hanoglu@panates.com>
2023-05-16 17:12:16 +0300

    Optimized build

---
+++ support/postbuild.cjs
@@ -0,0 +1,11 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+function clearPackageJson() {
+  const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'));
+  delete json.scripts;
+  delete json.devDependencies;
+  fs.writeFileSync(path.resolve(__dirname, '../build/package.json'), JSON.stringify(json, undefined, 2));
+}
+
+clearPackageJson();
