440de38eb023533a69d4ef14803f073b10caddb9
Eray Hanoğlu <e.hanoglu@panates.com>
2024-10-15 22:47:44 +0300

    chore: Updated config

--- test/_support/env.ts
+++ test/_support/env.ts
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = 'test';
 process.env.PGUSER = process.env.PGUSER || 'postgres';
-process.env.PGPASSWORD = process.env.PGPASSWORD || '';
+process.env.PGPASSWORD = process.env.PGPASSWORD || 'postgres';
 process.env.PGDATABASE = process.env.PGDATABASE || 'postgres';
 process.env.PGSCHEMA = process.env.PGSCHEMA || 'test';
